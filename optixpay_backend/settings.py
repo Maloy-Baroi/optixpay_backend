88ec5218cab941bf4ef023c5dfb09be911ec2efe
maloy-baroi <maloy.virat@gmail.com>
2024-12-20 00:24:26 +0600

    settings.py updated

--- optixpay_backend/settings.py
+++ optixpay_backend/settings.py
@@ -23,7 +23,7 @@ if not DEBUG:
     DOMAINS = ast.literal_eval(config('DOMAINS', default='[]'))
     ALLOWED_HOSTS = DOMAINS
 else:
-    ALLOWED_HOSTS = ['*', '147.79.66.187']
+    ALLOWED_HOSTS = ['147.79.66.187', 'localhost']
 
 # Application definition
 
