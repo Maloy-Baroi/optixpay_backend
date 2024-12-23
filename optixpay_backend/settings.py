a6f59fd65f9d84862fc1db2429269647036b3e9e
maloy-baroi <maloy.virat@gmail.com>
2024-12-24 01:50:49 +0600

    Bugfix: settings.py updated

--- optixpay_backend/settings.py
+++ optixpay_backend/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'optixpay_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 if not DEBUG:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': config('DB_NAME'),
-    #         'USER': config('DB_USER'),
-    #         'PASSWORD': config('DB_PASSWORD'),
-    #         'HOST': config('DB_HOST'),
-    #         'PORT': config('DB_PORT', '5432'),
-    #     }
-    # }
     DATABASES = {
-            'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT', '5432'),
         }
     }
+    # DATABASES = {
+    #         'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
 else:
     DATABASES = {
             'default': {
