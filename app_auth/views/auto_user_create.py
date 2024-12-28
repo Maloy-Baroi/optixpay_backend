a20517c5d39174be962f097a333fc85635a1733b
maloy-baroi <maloy.virat@gmail.com>
2024-12-28 18:01:56 +0600

    Feedback: Deposit serializer and view changed

--- app_auth/views/auto_user_create.py
+++ app_auth/views/auto_user_create.py
@@ -13,32 +13,33 @@ from utils.common_response import CommonResponse
 class AutoCreateUserView(APIView):
 
     def post(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        username = request.data.get('username')
-        password = request.data.get('password')
-        group_name = request.data.get('group')
-
-        if not all([email, username, password, group_name]):
-            return CommonResponse("error", {"message": "All fields are required."}, status_code=status.HTTP_400_BAD_REQUEST)
-
         try:
-            # Check if the group exists
+            email = request.data.get('email')
+            username = request.data.get('username')
+            password = request.data.get('password')
+            group_name = request.data.get('group')
+
+            if not all([email, username, password, group_name]):
+                CommonResponse("error", {"message": "All fields are required."},
+                                      status_code=status.HTTP_400_BAD_REQUEST)
+
             group, create = Group.objects.get_or_create(name=group_name)
-        except Group.DoesNotExist:
-            return CommonResponse("error", {"message": "Group not found."}, status_code=status.HTTP_404_NOT_FOUND)
 
-        try:
-            # Create the user and set their password
-            user = CustomUser.objects.create_user(email=email, username=username, password=password)
-            user.groups.add(group)
-            user.save()
-        except IntegrityError:
-            return CommonResponse("error", {"message": "A user with that email or username already exists."},
-                            status_code=status.HTTP_409_CONFLICT)
+            try:
+                # Create the user and set their password
+                user = CustomUser.objects.create_user(email=email, username=username, password=password)
+                user.groups.add(group)
+                user.save()
+                # Serialize the user object
+                data = CustomUserSerializer(user).data
 
-        # Serialize the user object
-        data = CustomUserSerializer(user).data
+                CommonResponse("success", data=data, status_code=status.HTTP_201_CREATED)
+            except IntegrityError:
+                CommonResponse("error", {"message": "A user with that email or username already exists."},
+                                      status_code=status.HTTP_409_CONFLICT)
 
-        return CommonResponse("success", data=data, status_code=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(str(e))
+            CommonResponse("error", {"message": str(e)}, status.HTTP_400_BAD_REQUEST)
 
 
