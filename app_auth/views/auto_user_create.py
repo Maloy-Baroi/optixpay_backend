1ca7bb4219fb09667101a1da4938a3d51816e23e
maloy-baroi <maloy.virat@gmail.com>
2024-12-28 17:54:03 +0600

    Feedback: Deposit serializer and view changed

--- app_auth/views/auto_user_create.py
+++ app_auth/views/auto_user_create.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Group
+from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,14 +9,36 @@ from app_auth.serializers.users import CustomUserSerializer
 from utils.common_response import CommonResponse
 
 
+
 class AutoCreateUserView(APIView):
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         email = request.data.get('email')
+        username = request.data.get('username')
+        password = request.data.get('password')
+        group_name = request.data.get('group')
 
-        user = CustomUser.objects.get(email=email, is_active=True)
-        user.set_password("123456")
-        user.save()
+        if not all([email, username, password, group_name]):
+            return CommonResponse("error", {"message": "All fields are required."}, status_code=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Check if the group exists
+            group = Group.objects.get(name=group_name)
+        except Group.DoesNotExist:
+            return CommonResponse("error", {"message": "Group not found."}, status_code=status.HTTP_404_NOT_FOUND)
+
+        try:
+            # Create the user and set their password
+            user = CustomUser.objects.create_user(email=email, username=username, password=password)
+            user.groups.add(group)
+            user.save()
+        except IntegrityError:
+            return CommonResponse("error", {"message": "A user with that email or username already exists."},
+                            status_code=status.HTTP_409_CONFLICT)
+
+        # Serialize the user object
         data = CustomUserSerializer(user).data
-        return CommonResponse("success", data, status.HTTP_201_CREATED)
+
+        return CommonResponse("success", data=data, status_code=status.HTTP_201_CREATED)
+
 
--- utils/common_response.py
+++ utils/common_response.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 
 
-def CommonResponse(type, data=None, status_code=None):
-    if type == "error":
+def CommonResponse(status_type, data=None, status_code=None):
+    if status_type == "error":
         return Response({"status": False, data: data}, status=status_code)
     else:
         return Response({"status": True, data: data}, status=status_code)
