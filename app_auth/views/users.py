04e6d2f7ba1005c25a10e3b386e5579365c1aff0
maloy-baroi <maloy.virat@gmail.com>
2024-12-31 01:08:56 +0600

    Feedback: User Response Change

--- app_auth/views/users.py
+++ app_auth/views/users.py
@@ -18,7 +18,6 @@ from services.isActiveUser import IsUserActive, is_user_active
 from services.send_main import send_verification_email
 from utils.common_response import CommonResponse
 
-
 # Create User
 import threading
 from django.core.mail import send_mail
@@ -40,10 +39,12 @@ class UserRegistrationView(APIView):
                 email_process = Process(target=send_verification_email, args=(user.email, otp))
                 email_process.start()
 
-                return CommonResponse("success", {'message': 'User registered. Please check your email for verification code.'}, status=status.HTTP_201_CREATED)
-            return CommonResponse("error", serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("success", {}, status.HTTP_201_CREATED,
+                                      'User registered. Please check your email for verification code.')
+            return CommonResponse("error", serializer.errors, status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return CommonResponse("error", {'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, str(e))
+
 
 class ResendVerifyOTPView(APIView):
     def post(self, request):
@@ -52,12 +53,12 @@ class ResendVerifyOTPView(APIView):
             user = CustomUser.objects.get(email=email)
 
             if not user:
-                return Response({"message": "No user found with this email!"}, status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "No User found with this email")
 
             otp_cached = UserVerificationToken.objects.get(user=user)
 
             if otp_cached is None:
-                return Response({"message": "No User found with this email"}, status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "No User found with this email")
 
             otp = random.randint(1000, 9999)
 
@@ -68,10 +69,10 @@ class ResendVerifyOTPView(APIView):
             email_process = Process(target=send_verification_email, args=(user.email, otp))
             email_process.start()
 
-            return CommonResponse("success", {'message': 'Please check your email for resend verification code.'},
-                            status=status.HTTP_201_CREATED)
+            return CommonResponse("success", {},
+                                  status.HTTP_201_CREATED, 'Please check your email for resend verification code.')
         except Exception as e:
-            return CommonResponse("error", {"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, str(e))
 
 
 # views.py
@@ -85,21 +86,20 @@ class VerifyOTPView(APIView):
             otp_cached = UserVerificationToken.objects.get(user=user)
 
             if otp_cached is None:
-                return CommonResponse("error", {'error': 'OTP has expired or does not exist.'},
-                                      status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("error", {},
+                                      status.HTTP_400_BAD_REQUEST, 'OTP has expired or does not exist.')
 
             if otp_provided == otp_cached.token:
                 user.is_active = True
                 agent_group, created = Group.objects.get_or_create(name='agent')  # Ensure the agent group exists
                 user.groups.add(agent_group)  # Add the user to the agent group
                 user.save()
-                return CommonResponse("success", {'message': 'Email verified successfully!'},
-                                      status=status.HTTP_200_OK)
-            return CommonResponse("error", {'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("success", {},
+                                      status.HTTP_200_OK, 'Email verified successfully!')
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, 'Invalid OTP')
 
         except Exception as e:
-            return CommonResponse("error", {"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, str(e))
 
 
 # Login
@@ -112,7 +112,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
         try:
             if not is_user_active(email):
-                return CommonResponse("error", {"error": "User is not active."}, status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "User is not active.")
 
             # Retrieve user by email
             user = CustomUser.objects.get(email=email)
@@ -132,18 +132,19 @@ class CustomTokenObtainPairView(TokenObtainPairView):
                     'refresh': str(refresh),
                     'access': access_token,
                     'groups': groups,
-                    'username': user.name
+                    'username': user.username,
+                    'email': user.email
                     # 'permissions': permission_serializers
-                }, status=status.HTTP_200_OK)
+                }, status.HTTP_200_OK, "Login Successful!")
                 # If password is correct, proceed to issue the token
                 # return super().post(request, *args, **kwargs)
             else:
                 # If password is incorrect, return an error response
-                return CommonResponse("error", {'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
+                return CommonResponse("error", {}, status.HTTP_401_UNAUTHORIZED, 'Invalid password')
 
         except CustomUser.DoesNotExist:
             # If the user does not exist, return an error response
-            return CommonResponse("error", {'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+            return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, 'User not found')
 
 
 class CustomTokenRefreshView(TokenRefreshView):
@@ -154,15 +155,15 @@ class CustomTokenRefreshView(TokenRefreshView):
 
         try:
             if refresh_token is None:
-                return CommonResponse("error", {'error': 'Refresh token is required'},
-                                      status=status.HTTP_400_BAD_REQUEST)
+                return CommonResponse("error", {},
+                                      status.HTTP_400_BAD_REQUEST, 'Refresh token is required')
 
             refresh = RefreshToken(refresh_token)
             access_token = str(refresh.access_token)
 
             return CommonResponse("success", {
                 'access': access_token,
-            }, status=status.HTTP_200_OK)
+            }, status.HTTP_200_OK, "Refresh Token Generated Successfully")
 
         except TokenError as e:
-            return CommonResponse("error", {'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "Token Error!")
