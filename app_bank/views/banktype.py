42168351f41d06ba133bf28dcfa91f3cf9bd8ddd
Maloy-Baroi <maloy.virat@gmail.com>
2025-01-08 18:38:48 +0600

    Feature: bank type updated

--- app_bank/views/banktype.py
+++ app_bank/views/banktype.py
@@ -1,22 +1,72 @@
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from app_bank.models.bank import BankTypeModel
 from app_bank.serializers.banktype import BankTypeModelSerializer
+from services.pagination import CustomPagination
 from utils.common_response import CommonResponse
 
 
 class BankTypeListAPIView(APIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
 
     def get(self, request):
         try:
-            bank_type = BankTypeModel.objects.all()
-            serializer = BankTypeModelSerializer(bank_type, many=True)
-            return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully get bank type")
-        except BankTypeModel.DoesNotExist:
-            return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Data not found.")
+            # Extract query parameters
+            page = request.query_params.get('page', 1)
+            page_size = request.query_params.get('page_size', self.pagination_class.page_size)
+            bank_type_id = request.query_params.get('bank_type_id')
+            search_query = request.query_params.get('search', '')
+            search_status = request.query_params.get('status', '')
+            bank = request.query_params.get('bank', '')
+            is_active = request.query_params.get('is_active', True)
+
+            # Filter bank_types based on query parameters
+            bank_types = BankTypeModel.objects.all()
+
+            if bank_type_id:
+                try:
+                    bank_type = BankTypeModel.objects.get(id=bank_type_id)
+                    bank_types_serializers = BankTypeModelSerializer(bank_type)
+                    return CommonResponse(
+                        "success", bank_types_serializers.data, status.HTTP_200_OK, "Data Found!"
+                    )
+                except BankTypeModel.DoesNotExist:
+                    return CommonResponse("error", "bank_type not found", status.HTTP_204_NO_CONTENT)
+
+            if search_query:
+                bank_types = bank_types.filter(
+                    Q(name__icontains=search_query) | Q(unique_id__icontains=search_query)
+                )
+            if search_status:
+                bank_types = bank_types.filter(status=search_status)
+            if bank:
+                bank_types = bank_types.filter(bank__icontains=bank)
+            if is_active:
+                bank_types = bank_types.filter(is_active=True)
+
+            if not bank_types.exists():
+                return CommonResponse("error", "No bank_types found", status.HTTP_204_NO_CONTENT)
+
+            # Apply pagination
+            paginator = self.pagination_class()
+            paginator.page_size = int(page_size)  # Override page size if provided
+            result_page = paginator.paginate_queryset(bank_types, request)
+
+            if result_page is not None:
+                bank_types_serializers = BankTypeModelSerializer(result_page, many=True)
+                return paginator.get_paginated_response(bank_types_serializers.data)
+            else:
+                return CommonResponse("error", "No bank_types available", status.HTTP_204_NO_CONTENT)
+
+        except Exception as e:
+            return CommonResponse(
+                "error", str(e), status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "An error occurred while retrieving bank_type data"
+            )
 
     def post(self, request):
         serializer = BankTypeModelSerializer(data=request.data, context={'request': request})
