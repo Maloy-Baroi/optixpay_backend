1778871cac7189284943d0b630e17c4851558288
Maloy-Baroi <maloy.virat@gmail.com>
2025-01-09 11:52:42 +0600

    Feature: Currency APIView updated

--- app_deposit/urls.py
+++ app_deposit/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from app_deposit.views.deposit import DepositAPIView, DepositListAPIView
-from app_deposit.views.currency import CurrencyAPIView, CurrencyListPostAPIView
+from app_deposit.views.currency import CurrencyUpdateAPIView, CurrencyListPostAPIView, CurrencyDeleteAPIView
 
 urlpatterns = [
     path('currency/', CurrencyListPostAPIView.as_view(), name='currency-list'),   # List and Create
-    path('currency/<int:pk>/', CurrencyAPIView.as_view(), name='currency-detail'),  # Retrieve, Update, Delete
+    path('currency/update/<int:pk>/', CurrencyUpdateAPIView.as_view(), name='currency-detail'),  # Retrieve, Update, Delete
+    path('currency/delete/<int:pk>/', CurrencyDeleteAPIView.as_view(), name='currency-delete'),
 
 
     path('deposits/', DepositListAPIView.as_view(), name='deposit-list'),   # List and Create
--- app_deposit/views/currency.py
+++ app_deposit/views/currency.py
@@ -1,5 +1,7 @@
 from locale import currency
 
+from django.db.models import Q
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
@@ -17,59 +19,115 @@ class CurrencyListPostAPIView(APIView):
 
     def get(self, request):
         try:
-            queryset = Currency.objects.all()
+            page = request.query_params.get('page', 1)
+            page_size = request.query_params.get('page_size', self.pagination_class.page_size)
+            currency_id = request.query_params.get('currency_id')
+            search_query = request.query_params.get('search', '')
+            search_status = request.query_params.get('status', '')
+            bank = request.query_params.get('bank', '')
+            is_active = request.query_params.get('is_active', True)
+
+            # Filter currencys based on query parameters
+            currencys = Currency.objects.all()
+
+            if currency_id:
+                try:
+                    currency = Currency.objects.get(id=currency_id)
+                    currencys_serializers = CurrencySerializer(currency)
+                    return CommonResponse(
+                        "success", currencys_serializers.data, status.HTTP_200_OK, "Data Found!"
+                    )
+                except Currency.DoesNotExist:
+                    return CommonResponse("error", "Currency not found", status.HTTP_204_NO_CONTENT)
+
+            if search_query:
+                currencys = currencys.filter(
+                    Q(name__icontains=search_query) | Q(unique_id__icontains=search_query)
+                )
+            if search_status:
+                currencys = currencys.filter(status=search_status)
+            if bank:
+                currencys = currencys.filter(bank__icontains=bank)
+            if is_active:
+                currencys = currencys.filter(is_active=is_active)
+
+            if not currencys.exists():
+                return CommonResponse("error", "No Currency found", status.HTTP_204_NO_CONTENT)
+
+            # Apply pagination
             paginator = self.pagination_class()
-            result_page = paginator.paginate_queryset(queryset, request)
-            serializer = CurrencySerializer(result_page, many=True)
-            return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully fetched all currencies")
-        except Currency.DoesNotExist:
-            return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Deposit not found")
+            paginator.page_size = int(page_size)  # Override page size if provided
+            result_page = paginator.paginate_queryset(currencys, request)
+
+            if result_page is not None:
+                currencys_serializers = CurrencySerializer(result_page, many=True)
+                return paginator.get_paginated_response(currencys_serializers.data)
+            else:
+                return CommonResponse("error", "No currency available", status.HTTP_204_NO_CONTENT)
+
+        except Exception as e:
+            return CommonResponse(
+                "error", str(e), status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "An error occurred while retrieving merchant data"
+            )
+
+        #     queryset = Currency.objects.all()
+        #     paginator = self.pagination_class()
+        #     result_page = paginator.paginate_queryset(queryset, request)
+        #     serializer = CurrencySerializer(result_page, many=True)
+        #     return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully fetched all currencies")
+        # except Currency.DoesNotExist:
+        #     return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Deposit not found")
 
     def post(self, request):
         serializer = CurrencySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(created_by=request.user, updated_by=request.user, is_active=True)
-            return CommonResponse("success", serializer.data, status.HTTP_201_CREATED, "Successfully created new currency")
+            return CommonResponse("success", serializer.data, status.HTTP_201_CREATED,
+                                  "Successfully created new currency")
         else:
             return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, serializer.errors)
 
 
-
-class CurrencyAPIView(APIView):
+class CurrencyUpdateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request, pk=None):
         if pk:
             try:
-                currency = Currency.objects.get(pk=pk)
+                currency = Currency.objects.get(id=pk)
                 serializer = CurrencySerializer(currency)
                 return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully fetched currency")
             except Currency.DoesNotExist:
-                return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Deposit not found")
+                return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Currency not found")
         else:
-            currency = Currency.objects.all()
-            serializer = CurrencySerializer(currency, many=True)
-            return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully fetched currency")
+            currencies = Currency.objects.all()
+            serializer = CurrencySerializer(currencies, many=True)
+            return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully fetched all currencies")
 
     def put(self, request, pk=None):
+        if not pk:
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "Missing currency identifier")
         try:
             currency = Currency.objects.get(pk=pk)
-            if not currency:
-                return CommonResponse("error", status.HTTP_404_NOT_FOUND, "Currency not found")
-            serializer = CurrencySerializer(currency, data=request.data,context={'request': request})
+            serializer = CurrencySerializer(currency, data=request.data, context={'request': request})
             if serializer.is_valid():
                 serializer.save()
                 return CommonResponse("success", serializer.data, status.HTTP_200_OK, "Successfully updated currency")
-            return CommonResponse("error", serializer.errors, status.HTTP_400_BAD_REQUEST, serializer.errors)
+            return CommonResponse("error", serializer.errors, status.HTTP_400_BAD_REQUEST, "Invalid data")
         except Currency.DoesNotExist:
             return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Currency not found")
 
 
+class CurrencyDeleteAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def delete(self, request, pk=None):
+        if not pk:
+            return CommonResponse("error", {}, status.HTTP_400_BAD_REQUEST, "Missing currency identifier")
         try:
             currency = Currency.objects.get(pk=pk, is_active=True)
-            if currency is None:
-                return CommonResponse("error", {}, status.HTTP_204_NO_CONTENT, "Currency Not Found!")
             currency.soft_delete()
-            return CommonResponse("success", {}, status.HTTP_204_NO_CONTENT, "Currency deleted successfully")
+            return CommonResponse("success", {}, status.HTTP_200_OK, "Currency deleted successfully")
         except Currency.DoesNotExist:
             return CommonResponse("error", {}, status.HTTP_404_NOT_FOUND, "Currency not found")
