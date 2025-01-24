038d8173326e463a73b2cf987e8816059ec0780f
maloy-baroi <maloy.virat@gmail.com>
2025-01-25 00:47:45 +0600

    Feature: Currency model Update Done

--- app_deposit/models/deposit.py
+++ app_deposit/models/deposit.py
@@ -6,8 +6,8 @@ from core.models.BaseModel import BaseModel
 
 class Currency(BaseModel):
 
-    name = models.CharField(max_length=255, unique=True)
-    currency_code =models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    currency_code =models.CharField(max_length=255)
     currency_symbol = models.CharField(max_length=255)
 
     class Meta:
